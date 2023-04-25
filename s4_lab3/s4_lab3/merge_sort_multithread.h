14ce1576efa194b62b3055fe9144dc194e5d29e9
Dm <dmytromk@gmail.com>
2023-04-26 00:26:12 +0300

    added multithreading implementation of merge sort

---
+++ s4_lab3/s4_lab3/merge_sort_multithread.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "merge_sort.h"
+
+#include <thread>
+
+template <typename main_iter, typename auxil_iter>
+void mergesort_do_multithread(const main_iter left, const main_iter right, const auxil_iter aux_left, int cur_thread_amount, int max_threads)
+{
+	const auto size = std::distance(left, right);
+
+	if (size < 2) {
+		return;
+	}
+
+	const auto middle = std::next(left, size / 2);
+	const auto aux_middle = std::next(aux_left, std::distance(left, middle));
+
+	if (cur_thread_amount < max_threads-1)
+	{
+		cur_thread_amount += 2;
+
+		std::thread th1(&mergesort_do_multithread<main_iter, auxil_iter>,
+			left, middle, aux_left, cur_thread_amount, max_threads);
+
+		std::thread th2(&mergesort_do_multithread<main_iter, auxil_iter>,
+			middle, right, aux_middle, cur_thread_amount, max_threads);
+
+		th1.join();
+		th2.join();
+	}
+
+	else
+	{
+		mergesort_do_multithread(left, middle, aux_left, cur_thread_amount, max_threads);
+		mergesort_do_multithread(middle, right, std::next(aux_left, std::distance(left, middle)), cur_thread_amount, max_threads);
+	}
+
+	merge(left, middle, right, aux_left);
+}
+
+template <typename main_iter>
+void mergesort_multithread(const main_iter left, const main_iter right, int max_threads = 8) //max amount of threads MUST BE EVEN
+{
+	std::vector<typename main_iter::value_type> buffer(std::distance(left, right));
+	mergesort_do_multithread(left, right, buffer.begin(), 0, max_threads);
+}
--- s4_lab3/s4_lab3/test.cpp
+++ s4_lab3/s4_lab3/test.cpp
@@ -1,11 +1,13 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "merge_sort.h"
+#include "merge_sort_multithread.h"
 #include "utils.h"
 
 #include "doctest.h"
 #include <time.h>
 
 TEST_CASE("Validity test: Merge sort is equal to std::sort") {
+	srand(time(0));
 	std::cout << "\nValidity test: Merge sort is equal to std::sort.\n";
 	for (int i = 2; i <= 10 * 100; i *= 100)
 	{
@@ -15,48 +17,89 @@ TEST_CASE("Validity test: Merge sort is equal to std::sort") {
 			auto test2 = test, test3 = test;
 
 			mergesort(test.begin(), test.end());
-			//mergesort_multithread(test2, 0, test.size() - 1);
+			mergesort_multithread(test2.begin(), test2.end());
 			std::sort(test3.begin(), test3.end());
-			//CHECK(test == test2);
+			CHECK(test == test2);
 			CHECK(test == test3);
 			std::cout << "\nSuccess";
 		}
 	}
 }
 
-TEST_CASE("\nSpeedtesting: merge sort, multithreaded merge sort, std::sort") {
+TEST_CASE("\nNEW Speedtesting: merge sort, multithreaded merge sort, std::sort") {
 	srand(time(0));
 	int start_size = 50;
 	int	end_size = 50000000;
-	std::clock_t start, end;
+	int number_of_threads = 8;
 
-	std::cout << "\n\nSpeedtesting merge sort, multithreaded merge sort, std::sort.\n\n";
+	std::cout << "\n\nNEW Speedtesting merge sort, multithreaded merge sort, std::sort.\n\n";
 
 	for (int i = start_size; i <= end_size; i *= 10)
 	{
 			std::vector<int> test = randomize(i, 2 * i);
 			auto test2 = test, test3 = test;
 			
-			/*start = clock();
-			mergesort(test.begin(), test.end());
-			end = clock();*/
-			auto mrg_sort_dur = time_evaluation(mergesort<std::vector<int>::iterator>, test.begin(), test.end());
-
-			/*start = clock();
-			mergesort_multithread(test2, 0, test2.size() - 1);
-			end = clock();
-			auto mrg_sort_multithread_dur = end - start;*/
-
-			/*start = clock();
-			std::sort(test3.begin(), test3.end());
-			end = clock();*/
-			auto std_sort_dur = time_evaluation(mergesort<std::vector<int>::iterator>, test3.begin(), test3.end());
+			auto mrg_sort_dur = time_evaluation(mergesort<std::vector<int>::iterator>, 
+				test.begin(), test.end());
+			auto mrg_sort_multithread_dur = time_evaluation(mergesort_multithread<std::vector<int>::iterator>, 
+				test2.begin(), test2.end(), number_of_threads);
+			auto std_sort_dur = time_evaluation(mergesort<std::vector<int>::iterator>, 
+				test3.begin(), test3.end());
 
 			CHECK(test == test3);
+			CHECK(test2 == test3);
 
-			int width = 25;
+			int width = 28;
 
-			std::cout << std::setw(10) << i << std::setw(width) << "Merge sort" << std::setw(width) << "std::sort\n";
-			std::cout << std::setw(width) << mrg_sort_dur << "ms" << std::setw(width) << std_sort_dur << "ms" << "\n\n";
+			std::cout << std::setw(5) << i << std::setw(width) << "Merge sort" 
+				<< std::setw(width) << "Merge multithread sort"
+				<< std::setw(width) << "std::sort\n"; 
+
+			std::cout << std::setw(width) << mrg_sort_dur << "ms" 
+				<< std::setw(width) << mrg_sort_multithread_dur << "ms"
+				<< std::setw(width) << std_sort_dur << "ms" << "\n\n";
 	}
 }
+
+/*TEST_CASE("\nOLD Speedtesting: merge sort, multithreaded merge sort, std::sort") {
+	//srand(time(0));
+	int start_size = 50;
+	int	end_size = 50000000;
+	std::clock_t start, end;
+
+	std::cout << "\n\nOLD Speedtesting merge sort, multithreaded merge sort, std::sort.\n\n";
+
+	for (int i = start_size; i <= end_size; i *= 10)
+	{
+		std::vector<int> test = randomize(i, 2 * i);
+		auto test2 = test, test3 = test;
+
+		start = clock();
+		mergesort(test.begin(), test.end());
+		end = clock();
+		auto mrg_sort_dur = end-start;
+
+		start = clock();
+		mergesort_multithread(test2.begin(), test2.end());
+		end = clock();
+		auto mrg_sort_multithread_dur = end - start;
+
+		start = clock();
+		std::sort(test3.begin(), test3.end());
+		end = clock();
+		auto std_sort_dur = end - start;
+
+		CHECK(test == test3);
+		CHECK(test2 == test3);
+
+		int width = 28;
+
+		std::cout << std::setw(5) << i << std::setw(width) << "Merge sort"
+			<< std::setw(width) << "Merge multithread sort"
+			<< std::setw(width) << "std::sort\n";
+
+		std::cout << std::setw(width) << mrg_sort_dur << "ms"
+			<< std::setw(width) << mrg_sort_multithread_dur << "ms"
+			<< std::setw(width) << std_sort_dur << "ms" << "\n\n";
+	}
+}*/
